fbeddc64a1ee036e787cdccd4a4ab4d8fae977a0
Jason Feinberg <github@redlxx.com>
2014-09-25 17:22:44 -0400

    Dan Comments
    Updates Regarding Dan's Suggestions

--- starting.js
+++ starting.js
@@ -1,46 +1,36 @@
 $(window).load(function() {    
 
-			var theWindow        = $(window),
-				$bg              = $(".bg");
-				//aspectRatio      = $bg.width() / $bg.height();
-								
+	var theWindow        = $(window),
+		$bg              = $(".bg");					
 
-			function aspectRatio(){
-				var activePage = $.mobile.activePage;
-				var bgimg = activePage.find('.bg');
-				return bgimg.width()/bgimg.height();
-			}
+	function aspectRatio(){
+		var activePage = $.mobile.activePage;
+		var bgimg = activePage.find('.bg');
+		return bgimg.width()/bgimg.height();
+	}
 
-			function resizeBg() {
-		
-				if ( (theWindow.width() / theWindow.height()) < aspectRatio() ) {
-					$bg
-						.removeClass('bgwidth')
-						.addClass('bgheight');
-				} else {
-					$bg
-						.removeClass('bgheight')
-						.addClass('bgwidth');
-				}
-					
-			}
-			theWindow.resize(resizeBg).trigger("resize");
+	function resizeBg() {
 
-			/*theWindow.on("pagecontainerchange", function(evt,ui){
-				//console.log(ui);
-				//console.log($(ui.toPage.find('img')));
-				var newimg = $(ui.toPage.find('img'));
-				if ((theWindow.width() / theWindow.height()) < aspectRatio){
-					newimg.height('100%');
-				}
-				newimg.width('100%');
-				newimg.height('100%');
-				newimg.attr('position','fixed');
-				
-				theWindow.resize(resizeBg).trigger("resize");
-
-			});*/
-
+		if ( (theWindow.width() / theWindow.height()) < aspectRatio() ) {
+			$bg
+				.removeClass('bgwidth')
+				.addClass('bgheight');
+		} else {
+			$bg
+				.removeClass('bgheight')
+				.addClass('bgwidth');
+		}
 			
+	}
+	theWindow.resize(resizeBg).trigger("resize");
 
+	var currPageId = $.mobile.activePage.attr('id');
+
+	$('#'+currPageId+'-link').addClass('currentpage').removeClass('otherpage');
+
+	$('.otherpage').on('click',function(){
+		$this=$(this);
+		$('.currentpage').removeClass('currentpage').addClass('otherpage');
+		$this.addClass('currentpage').removeClass('otherpage');
+	});
 });
